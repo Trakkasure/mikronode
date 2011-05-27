7cbdae2d7d95f17be2f4ec6297c0a1032299a100
Brandon Myers <trakkasure@gmail.com>
2011-05-27 11:09:26 -0400

    Added a few more examples. Privatized variables in API objects.

--- examples/getIpList.js
+++ examples/getIpList.js
@@ -1,11 +1,12 @@
 var api = require('../lib/index.js');
      
-var connection = new api('192.168.0.1','admin','password');
+var connection = new api('192.168.0.1','admin','');
 connection.connect(function(conn) {
 
    var chan=conn.openChannel();
-
+   conn.closeOnDone(true);
    chan.write('/ip/address/print',function() {
+      chan.closeOnDone(true);
       chan.on('done',function(data) {
 
          var parsed = api.parseItems(data);
@@ -14,9 +15,6 @@ connection.connect(function(conn) {
             console.log('Interface/IP: '+item.interface+"/"+item.address);
          });
  
-         chan.close();
-         conn.close();
-
       }); 
    }); 
 }); 
