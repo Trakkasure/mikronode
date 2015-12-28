542a59ee16463d21be4b9f858f929bee38d917b3
George Joseph <george.joseph@fairview5.com>
2015-12-28 14:39:14 -0700

    hold

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,36 @@
 module.exports = function(grunt) {
 	var path = require('path');
 	var DOC_PATH = 'doc';
+	var git_branch = '';
+
 	grunt.initConfig({
 		pkg : grunt.file.readJSON('package.json'),
 
 		clean : {
 			docs : {
 				src : DOC_PATH
+			},
+			'gh-pages' : {
+				src : '.grunt/grunt-gh-pages'
+			}
+		},
+		copy : {
+			docs : {
+				src : 'Readme.md',
+				dest : 'doc/'
+			}
+		},
+
+		shell : {
+			'get-branch' : {
+				command : 'git rev-parse --abbrev-ref HEAD',
+				options : {
+					stdout : false,
+					callback : function(err, stdout, stderr, cb) {
+						git_branch = stdout.trim();
+						cb();
+					}
+				}
 			}
 		},
 
@@ -26,13 +50,35 @@ module.exports = function(grunt) {
 			}
 		},
 
+		'gh-pages' : {
+			options : {
+				push : false,
+			},
+			'docs' : {
+				options : {
+					base : 'doc',
+				},
+				src : [ '**/*', '../Readme.md' ]
+			},
+		}
+
 	});
 
 	// Load task libraries
-	[ 'grunt-contrib-clean', 'grunt-jsdoc', ].forEach(function(taskName) {
+	[ 'grunt-contrib-clean', 'grunt-jsdoc', 'grunt-gh-pages', 'grunt-contrib-copy', 'grunt-shell' ].forEach(function(
+			taskName) {
 		grunt.loadNpmTasks(taskName);
 	});
 
-	grunt.registerTask('docs', 'Create documentation for mikronode', [ 'clean:docs', 'jsdoc:docs' ]);
+	grunt.registerTask('fail-branch', function() {
+		if (!git_branch.startsWith('v')) {
+			grunt.log.writeln('Branch ' + git_branch + ' is not a release branch');
+			return false;
+		}
+	});
+
+	grunt.registerTask('docs', 'Create documentation for mikronode', [ 'clean:docs', 'copy:docs', 'jsdoc:docs' ]);
+	grunt.registerTask('pages', 'Create gh-pages', [ 'check-release-branch', 'clean:gh-pages', 'gh-pages:docs' ]);
+	grunt.registerTask('check-release-branch', [ 'shell:get-branch', 'fail-branch' ]);
 	grunt.registerTask('default', 'Default', [ 'docs' ]);
 };
