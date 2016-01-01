94dc725c3b8adbb7fca02abfc3d6f4fc25ded80c
George Joseph <george.joseph@fairview5.com>
2015-12-31 17:10:06 -0700

    Add TLS Support
    The Connection options parameter now takes a 'tls' property that
    when set to true or a tls.TLSSocket options object, will connect
    to the device using TLS.  The default port of 8729 will be used
    unless overridden.

--- test/test1.js
+++ test/test1.js
@@ -12,7 +12,10 @@ function VLAN(vlan) {
 var connection = new MikroNode.Connection(process.argv[2], process.argv[3], process.argv[4], {
 	closeOnDone : true,
 	closeOnTimeout : true,
-	timeout : 4
+	timeout : 4,
+	tls : {
+		rejectUnauthorized : false
+	}
 });
 
 var connPromise = connection.getConnectPromise().then(function connected(conn) {
@@ -22,6 +25,8 @@ var connPromise = connection.getConnectPromise().then(function connected(conn) {
 	process.exit(99);
 });
 
+var availableVlan = -1;
+
 function getVlans() {
 	var p = connection.getCommandPromise('/interface/vlan/print', {
 		closeOnDone : false,
@@ -30,7 +35,6 @@ function getVlans() {
 	});
 	p.then(function(values) {
 		console.log('Got vlans');
-		var availableVlan = -1;
 		for (var vlan = 3000; vlan < 4000; vlan++) {
 			if (!values[vlan]) {
 				availableVlan = vlan;
@@ -41,7 +45,7 @@ function getVlans() {
 			console.error('No vlans available');
 			process.exit(99);
 		}
-		addVlan(availableVlan);
+		addVlan();
 		p.channel.close();
 
 	});
@@ -52,9 +56,10 @@ function getVlans() {
 	});
 }
 
-function addVlan(vlan) {
-	console.log('Adding vlan ' + vlan);
-	var p = connection.getCommandPromise([ '/interface/vlan/add', '=interface=ether1', '=vlan-id=' + vlan ], {
+function addVlan() {
+	console.log('Adding vlan ' + availableVlan);
+	var p = connection.getCommandPromise([ '/interface/vlan/add', '=name=vlan' + availableVlan, '=interface=ether1',
+			'=vlan-id=' + availableVlan ], {
 		closeOnDone : false
 	});
 	p.then(function(values) {
@@ -74,6 +79,7 @@ function listVlans() {
 		closeOnDone : false
 	});
 	p.then(function(values) {
+		console.log(values);
 		listen();
 		p.channel.close();
 	});
@@ -89,20 +95,24 @@ var listenChannelId;
 function listen() {
 	var c = connection.openChannel();
 	listenChannelId = c.id;
-	c.closeOnDone = true;
+	c.closeOnDone = false;
 	c.write('/ip/address/listen', function(channel) {
 		console.log('Listening to ip changes.');
 
 		console.log('Press CTRL-C to stop listening.');
 		channel.on('done', function() {
 			console.log('ip listen done');
+			channel.close();
 		});
 		channel.on('trap', function(result) {
 			console.log(result + ': SUCCESS!!');
+			cleanup();
+			channel.close();
 		});
 		channel.on('read', function(data) {
 			console.log('Heard: ' + data);
 			cancelListen();
+			channel.close();
 		}); // report when an IP is being addeed.
 		addIp();
 	});
@@ -111,16 +121,13 @@ function listen() {
 function addIp() {
 	console.log('Adding IP');
 	var p = connection.getCommandPromise('/ip/addr/add', {
-		'=interface' : 'ether1',
+		'=interface' : 'vlan' + availableVlan,
 		'=address' : '192.168.88.100/24'
 	}, {
-		closeOnDone : false
+		closeOnDone : true
 	});
-	p.then(function(values) {
-		p.channel.close();
-	});
+	p.then(function(values) {});
 	p['catch'](function(result) {
-		p.channel.close();
 		console.log('Add Address: ' + result.toString());
 		process.exit(99);
 	});
@@ -136,6 +143,56 @@ function cancelListen() {
 	p.then(function(values) {});
 	p['catch'](function(result) {
 		console.log('channel ' + p.channel.id + ': ' + result);
+	});
+}
+
+function cleanup() {
+	console.log('Cleaning up');
+
+	var pa = connection.getCommandPromise('/ip/address/print\n?address=192.168.88.100/24', {
+		closeOnDone : false
+	});
+
+	pa.then(function(values) {
+		console.log('Found IP: ', values[0]['.id']);
+		var da = connection.getCommandPromise('/ip/addr/remove', {
+			'=.id' : values[0]['.id'],
+		}, {
+			closeOnDone : true
+		});
+		da.then(function(values) {
+			console.log('Removed IP');
+		});
+
+		pa.channel.close(true);
+	});
+
+	pa['catch'](function(result) {
+		console.log(result);
+		pa.channel.close(true);
+	});
+
+	var pv = connection.getCommandPromise('/int/vlan/print\n?name=vlan' + availableVlan, {
+		closeOnDone : false
+	});
+
+	pv.then(function(values) {
+		console.log('Found VLAN: ', values[0]['.id']);
+		var dv = connection.getCommandPromise('/int/vlan/remove', {
+			'=.id' : values[0]['.id'],
+		}, {
+			closeOnDone : true
+		});
+		dv.then(function(values) {
+			console.log('Removed VLAN');
+		});
+
+		pv.channel.close(true);
+	});
+
+	pv['catch'](function(result) {
+		console.log(result);
+		pv.channel.close(true);
 	});
 
 }
